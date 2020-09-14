64c59c581828649c84e6a1b2a075921e94d0a61b
Harvey Le <harveyxcle@gmail.com>
2020-09-14 15:59:19 -0400

    adjust responsiveness for podcast and profile

--- src/Podcast/Podcast.js
+++ src/Podcast/Podcast.js
@@ -10,24 +10,25 @@ class Podcast extends React.Component {
     return (
       <div className='podcast'>
         <h2>Podcast</h2>
-        <div className='podcast-art'>
-          <div className='color-block'></div>
-          <img className='podcast-image' src={podcast} alt='podcast-artwork' />
-        </div>
-        
-        <div className='podcast-player'>
-          <Helmet>
-            <script src='https://www.buzzsprout.com/1276340.js?container_id=buzzsprout-large-player-1276340&player=large'></script>
-          </Helmet>
-          <div id='buzzsprout-large-player-1276340'></div>
-          <div className='social-icons'>
-            <a href='https://open.spotify.com/show/70iTFbizL1oC5lBe0Hd58J?si=bowHOc-YQ1-SWAoR_5Hj9w'>
-              <img className='social-icon' src={spotify} alt='spotify-icon' />
-            </a>
-            <a href='https://podcasts.apple.com/us/podcast/world-fantasy-football-alliance-wffa/id1529054571'>
-              <img className='social-icon' src={apple} alt='apple-icon' />
-            </a>
+        <div className='color-block' />
+        <div className='laptop-wrapper'>
+          <div className='podcast-art'>
+            <img className='podcast-image' src={podcast} alt='podcast-artwork' />
           </div>
+          <div className='podcast-player'>
+            <Helmet>
+              <script src='https://www.buzzsprout.com/1276340.js?container_id=buzzsprout-large-player-1276340&player=large'></script>
+            </Helmet>
+            <div id='buzzsprout-large-player-1276340'></div>
+          </div>
+        </div>
+        <div className='social-icons'>
+          <a href='https://open.spotify.com/show/70iTFbizL1oC5lBe0Hd58J?si=bowHOc-YQ1-SWAoR_5Hj9w'>
+            <img className='social-icon' src={spotify} alt='spotify-icon' />
+          </a>
+          <a href='https://podcasts.apple.com/us/podcast/world-fantasy-football-alliance-wffa/id1529054571'>
+            <img className='social-icon' src={apple} alt='apple-icon' />
+          </a>
         </div>
       </div>
     )
