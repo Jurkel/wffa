c2d1a46813985d883a812b5ea487991b4547db14
Harvey Le <harveyxcle@gmail.com>
2020-09-28 09:11:51 -0400

    Fine tuning some last minute responsiveness

--- src/Roster/Roster.js
+++ src/Roster/Roster.js
@@ -26,13 +26,14 @@ class Roster extends React.Component {
 
     const managers = this.props.managerData.map((list, index) => 
     <li key={index}>
-      <img src={require(`../images/${list.firstname}.png`)} alt={list.display_name} />
       <Link 
         style={{
           paddingLeft: 13,
           textDecoration: 'none',
           color: '#F2F2F2',
-          fontWeight: 'bold'
+          fontWeight: 'bold',
+          fontSize: 24,
+          background: 'transparent'
         }}
         to={{
           pathname: `/${list.display_name}`,
@@ -44,7 +45,8 @@ class Roster extends React.Component {
         onClick={() => {this.props.clickMe(list.owner_id)}}
         
       >
-          {list.display_name} 
+        <img src={require(`../images/${list.firstname}.png`)} alt={list.display_name} /><br />
+        {list.display_name} 
       </Link>
     </li>);
     
